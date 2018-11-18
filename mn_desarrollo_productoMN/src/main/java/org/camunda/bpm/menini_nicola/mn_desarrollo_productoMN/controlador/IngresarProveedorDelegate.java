fe2c7c03f5b582c559194655f3e30169d8578c7c
christian <christian@christian-nb>
2018-11-18 17:42:24 -0300

    Persiste proveedor dinamico en BD.

--- mn_desarrollo_productoMN/src/main/java/org/camunda/bpm/menini_nicola/mn_desarrollo_productoMN/controlador/IngresarProveedorDelegate.java
+++ mn_desarrollo_productoMN/src/main/java/org/camunda/bpm/menini_nicola/mn_desarrollo_productoMN/controlador/IngresarProveedorDelegate.java
@@ -2,7 +2,10 @@ package org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN.controlador;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN.logica.Fachada;
+import org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN.logica.IFachada;
 import org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN.modelo.ProductoMN;
+import org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN.valueObject.VOProductoMN;
 
 public class IngresarProveedorDelegate implements JavaDelegate {
 
@@ -13,6 +16,22 @@ public class IngresarProveedorDelegate implements JavaDelegate {
 		ProductoMN dataProductoMN = new ProductoMN();
 		dataProductoMN= (ProductoMN)execution.getVariable("dataProductoMN");
 		
+		//transformar objeto leido de pantalla a VO y persistirlo en BD
+		IFachada fachada= Fachada.getInstanciaSingleton();
+		VOProductoMN voProductoMN= new VOProductoMN();
+		
+		voProductoMN.setIdProductoMN(null);
+		voProductoMN.setTrabajoRealizado(dataProductoMN.getTrabajoRealizado());
+		voProductoMN.setCantidad(dataProductoMN.getCantidad());
+		voProductoMN.setNombre(dataProductoMN.getNombre());
+		voProductoMN.setMoneda(dataProductoMN.getMoneda());
+		voProductoMN.setTotal(dataProductoMN.getTotal());
+		voProductoMN.setIvaProducto(dataProductoMN.getIvaProducto());
+		voProductoMN.setProveedoresMN(dataProductoMN.getProveedoresMN());
+		//ACTUALMENTE ES NULL PERO ESTE VALOR DEBE SER TOMADO POR PANTALLA
+		voProductoMN.setIdClientePresupuesto(null);
+		
+		fachada.insertarProductoMN(voProductoMN);	
 		
 	}
 
