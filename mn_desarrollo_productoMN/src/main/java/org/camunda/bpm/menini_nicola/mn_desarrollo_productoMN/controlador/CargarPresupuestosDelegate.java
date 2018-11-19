fc3bd8a4757849f304cf5641ea4485e97e40e99c
christian <christian@christian-nb>
2018-11-19 15:58:17 -0300

    Revierte query. Agrega lista presupuesto, validaciones y quitar prov.

--- mn_desarrollo_productoMN/src/main/java/org/camunda/bpm/menini_nicola/mn_desarrollo_productoMN/controlador/CargarPresupuestosDelegate.java
+++ mn_desarrollo_productoMN/src/main/java/org/camunda/bpm/menini_nicola/mn_desarrollo_productoMN/controlador/CargarPresupuestosDelegate.java
@@ -1,10 +1,14 @@
 package org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN.controlador;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.value.ObjectValue;
 import org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN.logica.Fachada;
 import org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN.logica.IFachada;
 import org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN.modelo.Presupuesto;
@@ -14,12 +18,29 @@ public class CargarPresupuestosDelegate implements JavaDelegate{
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		
-		//cargar presupuestos aprobados
+		//levantar lista de presupuestos aprobados para productosMN desde BD
 		IFachada fachada= Fachada.getInstanciaSingleton();
 		List<Presupuesto> presupuestosAprobados= new ArrayList<Presupuesto>();
 		presupuestosAprobados= fachada.selectPresupuestosAprobados();
 		
-		//serializar lista de presupuestos a Json
+		//convertir lista de presupuestos a HashMap 
+	    //(el formulario de SDK trabaja con HashMap)
+		Map<Integer, String> presupuestosAprobadosMap= new HashMap<Integer, String>();
+		Integer i=0;
+		while(i < presupuestosAprobados.size())
+		{
+			presupuestosAprobadosMap.put(i, presupuestosAprobados.get(i).getCotizacion());
+			i++;
+		}
+		
+		//serializar datos de HashMap a json
+	    ObjectValue presupuestosDataValue = Variables.objectValue(presupuestosAprobadosMap)
+                .serializationDataFormat(Variables.SerializationDataFormats.JSON)
+                .create();
+	    
+	    //bindear HashMap serializado (a JSON) sobre el control 'select' del formulario ingresarProveedor-form
+	    execution.setVariable("PRESUPUESTOS_APROBADOS", presupuestosDataValue);
+	  	   
 		
 	}
 
--- mn_desarrollo_productoMN/src/test/java/org/camunda/bpm/menini_nicola/mn_desarrollo_productoMN/TestPresupuesto.java
+++ mn_desarrollo_productoMN/src/test/java/org/camunda/bpm/menini_nicola/mn_desarrollo_productoMN/TestPresupuesto.java
@@ -1,7 +1,9 @@
 package org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN.modelo.Presupuesto;
 import org.camunda.bpm.menini_nicola.mn_desarrollo_productoMN.persistencia.DAOPresupuesto;
@@ -29,6 +31,18 @@ public class TestPresupuesto {
 			System.out.println("condiciones venta: "+presupuestoAprobado.getCondicionesVenta());
 			System.out.println("descripcion: "+presupuestoAprobado.getDescripcion());
 		}
-	
+		
+		//prueba de conversión de lista de presupuestos a hashMap
+		Map<Integer, String> presupuestosAprobadosMap= new HashMap<Integer, String>();
+		Integer i=0;
+		while(i < presupuestosAprobados.size())
+		{
+			presupuestosAprobadosMap.put(i, presupuestosAprobados.get(i).getCotizacion());
+			i++;
+		}
+		
+		//mostrar tamanio del hashMap por pantalla
+		System.out.println();
+		System.out.println("tamaño del hashMap: "+ presupuestosAprobadosMap.size());
 	}
 }
